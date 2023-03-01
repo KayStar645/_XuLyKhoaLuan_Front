ef7d24cc2118bcbb55feeb6bc119984d90eea216
thuan <thuanpt182@gmail.com>
2023-03-01 09:49:06 +0700

    chia component theo vai trò

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { HomeComponent } from './home/home.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { ShareComponent } from './share/share.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, DashboardComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, DashboardComponent, AdminComponent, ShareComponent],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [HttpClient, FormBuilder],
   bootstrap: [AppComponent]
