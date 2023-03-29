ab1f14fb8d8325a8101ceb9a3e3a4a280f0bea98
chuazz <anhsonbeo97@gmail.com>
2023-03-29 15:04:31 +0700

    Xông kế hoạch

--- src/app/ministry/ministry-nhiemvu/ministry-chitietnhiemvu/ministry-chitietnhiemvu.component.ts
+++ src/app/ministry/ministry-nhiemvu/ministry-chitietnhiemvu/ministry-chitietnhiemvu.component.ts
@@ -254,17 +254,19 @@ export class MinistryChitietnhiemvuComponent {
 
   checkDaySmaller() {
     let formValue: any = this.nvForm.form.value;
-    let ngayKt: any = this.nvForm.form.get('ngayKt');
+    let ngayKtControl: any = this.nvForm.form.get('ngayKt');
+    let ngayBd = new Date(dateVNConvert(this.ngayBd));
+    let ngayKt = new Date(dateVNConvert(formValue.ngayKt));
 
-    ngayKt.setValidators([
+    ngayKtControl.setValidators([
       this.sharedService.customValidator(
         'smallerDay',
         / /,
-        formValue.ngayKt > this.ngayBd ? true : false
+        ngayKt.getTime() > ngayBd.getTime() ? true : false
       ),
       Validators.required,
     ]);
-    ngayKt.updateValueAndValidity();
+    ngayKtControl.updateValueAndValidity();
     this.nvForm.validateSpecificControl(['ngayKt', 'thoiGianKt']);
   }
 
