825319c2d9b840d3ce895034055de3b019688ca0
thuan <thuanpt182@gmail.com>
2023-03-30 16:22:59 +0700

    Merge remote-tracking branch 'origin/student' GetChuyennganhByMaDT

--- src/app/services/deTai_chuyenNganh.service.ts
+++ src/app/services/deTai_chuyenNganh.service.ts
@@ -4,44 +4,75 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs/internal/Observable';
+import { ChuyenNganh } from '../models/ChuyenNganh.model';
 
 
 @Injectable({
-    providedIn: "root",
+  providedIn: 'root',
 })
 export class deTai_chuyenNganhService {
-    private apiUrl = environment.api;
+  private apiUrl = environment.api;
 
-    constructor(
-        private http: HttpClient,
-        private shareService: shareService
-        ){}
+  constructor(private http: HttpClient, private shareService: shareService) {}
 
-    async getAll(): Promise<DeTai_ChuyenNganh[]>{
-        return await this.http.get<DeTai_ChuyenNganh[]>(`${this.apiUrl}/api/DetaiChuyennganh`, 
-        this.shareService.httpOptions).toPromise() ?? [];
+  async getAll(): Promise<DeTai_ChuyenNganh[]> {
+    return (
+      (await this.http
+        .get<DeTai_ChuyenNganh[]>(
+          `${this.apiUrl}/api/DetaiChuyennganh`,
+          this.shareService.httpOptions
+        )
+        .toPromise()) ?? []
+    );
+  }
+
+  async getByMaDtMaCn(
+    MaDT: string,
+    MaCN: string
+  ): Promise<DeTai_ChuyenNganh[]> {
+    try {
+      var response: DeTai_ChuyenNganh[] = [];
+      response =
+        (await this.http
+          .get<DeTai_ChuyenNganh[]>(
+            `${this.apiUrl}/api/DetaiChuyennganh/maDT, maCN?maDT=${MaDT}&maCN=${MaCN}`,
+            this.shareService.httpOptions
+          )
+          .toPromise()) ?? (response as DeTai_ChuyenNganh[]);
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  }
 
-    async getByMaDtMaCn(MaDT: string, MaCN:string):Promise<DeTai_ChuyenNganh[]> {
-         try {
-            var response : DeTai_ChuyenNganh[] = [];
-            response = await this.http.get<DeTai_ChuyenNganh[]>(
-              `${this.apiUrl}/api/DetaiChuyennganh/maDT, maCN?maDT=${MaDT}&maCN=${MaCN}`,
-              this.shareService.httpOptions
-            ).toPromise() ?? response as DeTai_ChuyenNganh[];
-            return response;
-          } catch (error) {
-            console.error(error);
-            throw error;
-          }
-    }
+  async add(deTai_chuyenNganh: DeTai_ChuyenNganh): Promise<any> {
+    return await this.http
+      .post(
+        `${this.apiUrl}/api/DetaiChuyennganh`,
+        deTai_chuyenNganh,
+        this.shareService.httpOptions
+      )
+      .toPromise();
+  }
 
-    async add(deTai_chuyenNganh: DeTai_ChuyenNganh): Promise<any> {
-    return await this.http.post(`${this.apiUrl}/api/DetaiChuyennganh`, deTai_chuyenNganh, this.shareService.httpOptions).toPromise();
-    }
+  async delete(MaDT: string, MaCN: string): Promise<any> {
+    return await this.http
+      .delete(
+        `${this.apiUrl}/api/DetaiChuyennganh/maDT, maCN?maDT=${MaDT}&maCN=${MaCN}`,
+        this.shareService.httpOptions
+      )
+      .toPromise();
+  }
 
-    async delete(MaDT: string, MaCN:string): Promise<any> {
-    return await this.http.delete(`${this.apiUrl}/api/DetaiChuyennganh/maDT, maCN?maDT=${MaDT}&maCN=${MaCN}`, 
-    this.shareService.httpOptions).toPromise();
-    }
+  async GetChuyennganhByMaDT(maDt: string): Promise<ChuyenNganh[]> {
+    return (
+      (await this.http
+        .get<ChuyenNganh[]>(
+          `${this.apiUrl}/api/DetaiChuyennganh/maDT?maDT=${maDt}`,
+          this.shareService.httpOptions
+        )
+        .toPromise()) ?? []
+    );
+  }
 }
