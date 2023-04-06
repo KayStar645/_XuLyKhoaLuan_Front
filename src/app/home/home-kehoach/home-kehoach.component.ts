123334007583522bbd122e826c1d4aca8b0933b0
thuan <thuanpt.work@gmail.com>
2023-04-06 09:00:45 +0700

    xóa file scss kế hoạch

--- src/app/home/home-kehoach/home-kehoach.component.ts
+++ src/app/home/home-kehoach/home-kehoach.component.ts
@@ -4,7 +4,6 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-home-kehoach',
   templateUrl: './home-kehoach.component.html',
-  styleUrls: ['./home-kehoach.component.scss'],
 })
 export class HomeKehoachComponent {
   constructor(private titleService: Title) {}
