0067e9f807c5c1bf5f1cec8dc041f9c090bb53aa
thuan <thuanpt.work@gmail.com>
2023-04-12 21:58:03 +0700

    GetNhomByMadtAsync

--- src/app/services/nhom.service.ts
+++ src/app/services/nhom.service.ts
@@ -9,46 +9,90 @@ import { shareService } from './../services/share.service';
   providedIn: 'root',
 })
 export class nhomService {
-    private apiUrl = environment.api;
-    //private nhoms!: BehaviorSubject<Nhom>;
+  private apiUrl = environment.api;
+  //private nhoms!: BehaviorSubject<Nhom>;
 
-    constructor(private http: HttpClient, private router: Router,
-      private shareService: shareService) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private shareService: shareService
+  ) {}
 
-    async getAll(): Promise<Nhom[]> {
-      return await this.http.get<Nhom[]>(`${this.apiUrl}/api/Nhoms`, this.shareService.httpOptions).toPromise() ?? [];
+  async getAll(): Promise<Nhom[]> {
+    return (
+      (await this.http
+        .get<Nhom[]>(`${this.apiUrl}/api/Nhoms`, this.shareService.httpOptions)
+        .toPromise()) ?? []
+    );
+  }
+
+  async getById(MaNhom: string): Promise<Nhom> {
+    try {
+      var response = new Nhom();
+      response =
+        (await this.http
+          .get<Nhom>(
+            `${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${MaNhom}`,
+            this.shareService.httpOptions
+          )
+          .toPromise()) ?? (response as Nhom);
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  }
 
-    async getById(MaNhom: string):Promise<Nhom> {
-      try {
-        var response = new Nhom();
-        response = await this.http.get<Nhom>(
-          `${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${MaNhom}`,
+  async GetNhomByMadtAsync(MaDT: string): Promise<Nhom> {
+    try {
+      var response = new Nhom();
+      response =
+        (await this.http
+          .get<Nhom>(
+            `${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${MaDT}`,
+            this.shareService.httpOptions
+          )
+          .toPromise()) ?? (response as Nhom);
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async add(nhom: Nhom): Promise<any> {
+    return await this.http
+      .post(`${this.apiUrl}/api/Nhoms`, nhom, this.shareService.httpOptions)
+      .toPromise();
+  }
+
+  async update(nhom: Nhom): Promise<any> {
+    return await this.http
+      .put<any>(
+        `${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${nhom.maNhom}`,
+        nhom,
+        this.shareService.httpOptions
+      )
+      .toPromise();
+  }
+
+  async delete(MaNhom: string): Promise<any> {
+    return await this.http
+      .delete(
+        `${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${MaNhom}`,
+        this.shareService.httpOptions
+      )
+      .toPromise();
+  }
+
+  async countThanhVien(MaNhom: string): Promise<number> {
+    return (
+      (await this.http
+        .get<number>(
+          `${this.apiUrl}/api/Nhoms/ma?ma=${MaNhom}`,
           this.shareService.httpOptions
-        ).toPromise() ?? response as Nhom;
-        return response;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    }
-
-    async add(nhom: Nhom): Promise<any> {
-      return await this.http.post(`${this.apiUrl}/api/Nhoms`, nhom, this.shareService.httpOptions).toPromise();
-    }
-
-    async update(nhom: Nhom): Promise<any> {
-      return await this.http.put<any>(`${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${nhom.maNhom}`, 
-      nhom, this.shareService.httpOptions).toPromise();
-    }
-
-    async delete(MaNhom: string): Promise<any> {
-      return await this.http.delete(`${this.apiUrl}/api/Nhoms/MaNhom?MaNhom=${MaNhom}`, 
-      this.shareService.httpOptions).toPromise();
-    }
-
-    async countThanhVien(MaNhom: string): Promise<number> {
-      return await this.http.get<number>(`${this.apiUrl}/api/Nhoms/ma?ma=${MaNhom}`, 
-      this.shareService.httpOptions).toPromise() ?? 0;
-    }
+        )
+        .toPromise()) ?? 0
+    );
+  }
 }
