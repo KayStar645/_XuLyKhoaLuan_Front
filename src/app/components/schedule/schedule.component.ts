fddcf515fd05498a6aff82cebd7c6759611c7c2c
Chuazz <anhsonbeo97@gmail.com>
2023-04-17 14:43:37 +0700

    Xong giao diện lịch phản biện của giảng viên

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { endOfDay, startOfDay } from 'date-fns';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { endOfDay, format, startOfDay } from 'date-fns';
+import { DatePickerComponent } from 'ng2-date-picker';
 
 @Component({
   selector: 'app-schedule',
@@ -7,10 +8,16 @@ import { endOfDay, startOfDay } from 'date-fns';
   styleUrls: ['./schedule.component.scss'],
 })
 export class ScheduleComponent implements OnInit {
+  @ViewChild('dayPicker')
+  datePicker!: DatePickerComponent;
+
   @Input() data: any[] = [];
+
+  currDate: string = format(new Date(), 'dd-MM-yyyy');
 
   ngOnInit(): void {
     this.setSchedule();
+
   }
 
   setSchedule() {
